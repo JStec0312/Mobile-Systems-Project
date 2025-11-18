f9272ef4f23304237fcba8845911be9a642d5c0f
Jakub Stec <stecu03@gmail.com>
2025-11-18 22:25:31 +0100

    fake use cases

--- app/src/main/java/com/example/petcare/data/dto/TaskDto.kt
+++ app/src/main/java/com/example/petcare/data/dto/TaskDto.kt
@@ -1,30 +1,33 @@
 package com.example.petcare.data.dto
 
 import com.example.petcare.common.taskPriorityEnum
+import com.example.petcare.common.taskStatusEnum
 import com.example.petcare.common.taskTypeEnum
+import com.example.petcare.common.utils.DateConverter
 import com.example.petcare.domain.model.Task
-import com.google.firebase.Timestamp
 
 data class TaskDto(
     val id: String,
+    val petId : String,
     val type : taskTypeEnum? = taskTypeEnum.other,
     val title : String,
     val description : String? = null,
     val notes : String? = null,
+    val status: taskStatusEnum,
     val dueAt : String? = null,
-    val priorityEnum: taskPriorityEnum? = taskPriorityEnum.normal,
+    val priority: taskPriorityEnum? = taskPriorityEnum.normal,
     val createdAt: String? = null,
 ) {
     fun toModel(): Task{
         return Task(
-            id = TODO,
-            petId = TODO(),
-            type = TODO(),
-            title = TODO(),
-            notes = TODO(),
-            priority = TODO(),
-            status = TODO(),
-            createdAt = TODO()
+            id = this.id,
+            petId = this.petId,
+            type = this.type,
+            title = this.title,
+            notes = this.notes,,
+            priority = this.priority,
+            status = this.status,
+            createdAt = DateConverter.stringToLocalDate(this.createdAt)
         )
     }
 }
--- app/src/main/java/com/example/petcare/domain/model/Task.kt
+++ app/src/main/java/com/example/petcare/domain/model/Task.kt
@@ -3,16 +3,15 @@ package com.example.petcare.domain.model
 import com.example.petcare.common.taskPriorityEnum
 import com.example.petcare.common.taskStatusEnum
 import com.example.petcare.common.taskTypeEnum
-import kotlinx.datetime.Instant
-import java.util.UUID
+import kotlinx.datetime.LocalDate
 
 data class Task(
     val id: String,
     val petId: String,
-    val type: taskTypeEnum,
+    val type: taskTypeEnum?,
     val title: String,
     val notes: String?,
-    val priority: taskPriorityEnum = taskPriorityEnum.normal,
+    val priority: taskPriorityEnum? = taskPriorityEnum.normal,
     val status: taskStatusEnum = taskStatusEnum.planned,
-    val createdAt: Instant
+    val createdAt: LocalDate
 )

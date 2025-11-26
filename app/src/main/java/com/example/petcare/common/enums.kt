2cbb62f33e0dd9b75fb351b435b7504edc2bb503
Julia <250353@edu.p.lodz.pl>
2025-11-26 11:32:20 +0100

    dashboard

--- app/src/main/java/com/example/petcare/common/enums.kt
+++ app/src/main/java/com/example/petcare/common/enums.kt
@@ -2,7 +2,7 @@ package com.example.petcare.common
 
 enum class speciesEnum { dog, cat }
 enum class sexEnum { male, female, unknown }
-enum class taskTypeEnum { walk, training, grooming, vaccination, deworming, other }
+enum class taskTypeEnum { walk, training, grooming, vaccination, deworming, feeding, other }
 enum class taskStatusEnum { planned, done, skipped, cancelled }
 enum class taskPriorityEnum { low, normal, high }
 enum class notificationChannelEnum { tasks, meds, general }

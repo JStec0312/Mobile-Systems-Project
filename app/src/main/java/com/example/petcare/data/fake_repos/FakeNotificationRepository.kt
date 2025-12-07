18bb7e7406f75bac5593ce27882f39ee75a22e82
Jakub Stec <stecu03@gmail.com>
2025-12-07 23:37:48 +0100

    minor fix

--- app/src/main/java/com/example/petcare/data/fake_repos/FakeNotificationRepository.kt
+++ app/src/main/java/com/example/petcare/data/fake_repos/FakeNotificationRepository.kt
@@ -1,6 +1,10 @@
 package com.example.petcare.data.fake_repos
 
+import android.app.Notification
 import com.example.petcare.domain.repository.INotificationRepository
 
 class FakeNotificationRepository: INotificationRepository {
+    override fun getFutureNotifications(): List<Notification> {
+        TODO("Not yet implemented")
+    }
 }
